455956e3a1c3a9de8d1f22b9cb05482502624a32
Alisson Koerich <alisson2023014305@alunocesul.com>
2024-10-28 22:27:14 -0300

    feat: implementado a funcao de voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,27 @@
 import { Text, TouchableOpacity } from "react-native";
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
     showBackButton?: boolean;
 }
 
 export function Header({showBackButton = false }: Props){
+    
+    const navigation = useNavigation();
+
+    function handleGoBack(){
+        navigation.goBack();
+    }
+
+    
     return(
         <Container>
             {
                 showBackButton && 
-                <BackButton>
-                                
+                <BackButton onPress={handleGoBack}>
+                             
                 <BackIcon name="arrow-back-ios"/>
 
                 </BackButton>

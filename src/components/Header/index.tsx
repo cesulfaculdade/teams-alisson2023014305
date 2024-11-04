20d062db0b4d726ea7b7e47781d23e6e88ef89a9
Alisson Koerich <alisson2023014305@alunocesul.com>
2024-11-04 19:31:50 -0300

    refactor: alterada navegacao do componente header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ export function Header({showBackButton = false }: Props){
     const navigation = useNavigation();
 
     function handleGoBack(){
-        navigation.goBack();
+        navigation.navigate("teams");
     }
 
     
